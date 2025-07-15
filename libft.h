1eea2678f8c7961da5ff81cf48a6c52b92801009
nweber23 <nweber@student.42heilbronn.de>
2025-07-15 15:09:28 +0200

    added ft_sqrt

--- libft.h
+++ libft.h
@@ -31,6 +31,7 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+int		ft_sqrt(int n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strchr(const char *s, int c);
