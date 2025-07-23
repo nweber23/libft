0b528545ecb23376b2a2d9b6de2a4f7ddd1272cb
nweber23 <nweber@student.42heilbronn.de>
2025-07-23 22:30:51 +0200

    Update libft.h

--- libft.h
+++ libft.h
@@ -34,6 +34,7 @@ int		ft_tolower(int c);
 int		ft_sqrt(int n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
@@ -58,6 +59,7 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+double	ft_atof(const char *str);
 
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
